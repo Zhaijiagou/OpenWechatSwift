945f8a9803dbcb5f4db35617e993a2c0cbcc8abb
Hengmin Wu <i@moonagic.com>
2024-07-26 07:35:43 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
-      name: "WechatOpenSDK",
+      name: "OpenWechatSwift",
       targets: ["WechatOpenSDK"]
     ),
   ],
