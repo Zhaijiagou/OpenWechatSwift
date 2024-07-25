305fe28c95337b0bb2637e3d741d1bb3fcd40081
Hengmin Wu <i@moonagic.com>
2024-07-26 07:39:24 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "OpenWechatSwift",
+  name: "WechatOpenSDK",
   platforms: [.iOS(.v13)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
